451e923e0260d8d4c95382db2306baccb7076048
Jose Ordoñez <131785570+JoseOrdonezB@users.noreply.github.com>
2023-10-16 07:30:44 -0600

    Update Pasador.java

--- Ejercicio5/Pasador.java
+++ Ejercicio5/Pasador.java
@@ -1,6 +1,6 @@
 class Pasador extends Jugador {
-    int pases; // Cantidad de pases
-    int fintas; // Cantidad de fintas
+    int pases;
+    int fintas;
 
     public Pasador(String nombre, String pais, int errores, int aces, int totalServicios, int pases, int fintas) {
         super(nombre, pais, errores, aces, totalServicios);
